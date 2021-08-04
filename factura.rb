ab06590dc666170fe78402dcf77845c706d53d60
Alejandro Rizzo <alejandro.rizzo@famar.com>
2021-08-03 22:36:35 -0300

    Check if quantity and unit price are valid numeric values

--- factura.rb
+++ factura.rb
@@ -5,11 +5,18 @@ STATE_TAX = {
 
 # Check for valid inputs
 if (ARGV.size != 3)
-    puts "Usage: ruby factura.rb <cantidad> <precio unitario> <estado>"
+    puts "Usage: ruby factura.rb <quantity> <unit price> <state>"
     exit
 end
 
+quantity = Float(ARGV[0]) rescue nil
+unit_price = Float(ARGV[1]) rescue nil
 state = ARGV[2]
+
+if ( (quantity == nil) || (unit_price == nil) )
+    puts "Both quantity and unit price need to be numeric"
+    exit
+end
 
 if (STATE_TAX[state] == nil)
     puts "Invalid state. Please provide a valid state from the following options:"
